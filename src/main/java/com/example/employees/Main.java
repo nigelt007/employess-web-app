939d55d40e36c5d0824a51509aec8a8cca410aa8
nigelt007 <nigelt007@gmail.com>
2018-12-05 09:58:57 +0530

    updated the code as per documentation

--- src/main/java/com/example/employees/Main.java
+++ src/main/java/com/example/employees/Main.java
@@ -3,17 +3,20 @@ package com.example.employees;
 
 import org.apache.catalina.startup.Tomcat;
 //Import the Optional class
+import java.util.Optional;
 
 public class Main {
     
     public static final Optional<String> PORT = Optional.ofNullable(System.getenv("PORT"));
     //Create a PORT instance variable
 	
+	
     public static void main(String[] args) throws Exception {
         String contextPath = "/" ;
         String appBase = ".";
         Tomcat tomcat = new Tomcat();  
-		//Use this PORT variable to set the port on Tomcat instance		
+		//Use this PORT variable to set the port on Tomcat instance
+        tomcat.setPort(Integer.valueOf(PORT.orElse("8080") ));		
         tomcat.getHost().setAppBase(appBase);
         tomcat.addWebapp(contextPath, appBase);
         tomcat.start();
